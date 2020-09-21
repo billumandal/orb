28e535ee38b51490617928e4563467ca6892ab42
billumandal <billu.mandal@gmail.com>
2020-09-21 12:36:19 +0530

    Initial upload of the project

---
+++ orb.py
@@ -0,0 +1,8 @@
+from nsepython import *
+
+#At 9:20 it will scan all fno stocks and take the first 10 and last 10.
+#highest gainer and loser of the day
+#from here https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES IN F%26O
+
+folist = fnolist()[3:]
+
