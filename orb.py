89ce67da735f2f5a31c3b22f4a5f0346cc2ba8ce
billumandal <billu.mandal@gmail.com>
2020-09-24 12:28:18 +0530

    some more jhamela added, still haven't been able to get how to add as a table to sort later

--- orb.py
+++ orb.py
@@ -5,13 +5,18 @@ from nsepython import *
 #from here https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES IN F%26O
 
 folist = fnolist()[3:]
+value_dict = {}
 
 for scrip in folist:
-    #price_of_scrip = nse_eq(scrip)['priceInfo']['close']
-    price_of_scrip = nse_quote_ltp(scrip) 
+    open_price = nse_eq(scrip)['priceInfo']['open']
+    high_price = nse_eq(scrip)['priceInfo']['intraDayHigh']
+    low_price = nse_eq(scrip)['priceInfo']['intraDayLow']
+    #price_of_scrip = nse_quote_ltp(scrip)
+    high_difference = high_price-open_price
+    low_difference = open_price-low_price
     
-    if price_of_scrip < float(upper_bound):
-        if price_of_scrip > float(lower_bound):
-            resultant_scrips.append(scrip)
+    #if price_of_scrip < float(upper_bound):
+    #    if price_of_scrip > float(lower_bound):
+    #        resultant_scrips.append(scrip)
 
 print(resultant_scrips)
