ca2734d4066bb1ab173b12dbc32687f37995c047
billumandal <billu.mandal@gmail.com>
2020-09-27 13:57:48 +0530

    written some nsefetch code, but that's not working

--- orb.py
+++ orb.py
@@ -6,24 +6,32 @@ import pandas as pd
 #highest gainer and loser of the day
 #from here https://www.nseindia.com/api/equity-stockIndices?index=SECURITIES IN F%26O
 
-folist = fnolist()[3:8]
-scrip_values = {}
+#folist = fnolist()[3:8]
+#scrip_values = {}
 
-for scrip in folist:
-    open_price = nse_eq(scrip)['priceInfo']['open']
-    high_price = nse_eq(scrip)['priceInfo']['intraDayHighLow']['max']
-    low_price = nse_eq(scrip)['priceInfo']['intraDayHighLow']['min']
+	#for scrip in folist:
+    #open_price = nse_eq(scrip)['priceInfo']['open']
+    #high_price = nse_eq(scrip)['priceInfo']['intraDayHighLow']['max']
+    #low_price = nse_eq(scrip)['priceInfo']['intraDayHighLow']['min']
     #price_of_scrip = nse_quote_ltp(scrip)
 
     #trying to put max positive and negative to be sorted in table
-    high_diff_percentage = (high_price-open_price)/open_price
-    low_diff_percentage = (low_price-open_price)/open_price
+    #high_diff_percentage = (high_price-open_price)/open_price
+    #low_diff_percentage = (low_price-open_price)/open_price
 
-    scrip_values.update({scrip : [high_diff_percentage, low_diff_percentage]})
+    #scrip_values.update({scrip : [high_diff_percentage, low_diff_percentage]})
 
     #if price_of_scrip < float(upper_bound):
     #    if price_of_scrip > float(lower_bound):
     #        resultant_scrips.append(scrip)
 
-df = pd.DataFrame(scrip_values)
-print(df)
+#df = pd.DataFrame(scrip_values)
+#print(df)
+
+    # Do the same with nsefetch.
+    scrip_list = []
+    folist = nsefetch('http://www.nserindia.com/api/equity-stockIndices?index=SECURITIES%20IN%20F%26O')
+    for scrip in folist:
+    	percentage_change = scrip['pChange']
+    	script_list.append(percentage_change)
+
